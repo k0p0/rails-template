9f1f605bfe568ae37edd3f21980fe81efc0ef91b
k0p0 <olivier@observices.fr>
2019-04-30 12:01:48 +0200

    Update full.rb

--- full.rb
+++ full.rb
@@ -228,6 +228,7 @@ after_bundle do
   # Generators: db + simple form + pages controller
   ########################################
   rails_command 'db:drop db:create db:migrate'
+  rails_command 'webpacker:install'
   generate('simple_form:install', '--bootstrap')
   generate(:controller, 'pages', 'home', '--skip-routes', '--no-test-framework')
 
